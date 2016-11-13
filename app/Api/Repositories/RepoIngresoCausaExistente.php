490589d420eeee26270f6b0f51db9078da209c2b
vagrant <vagrant@homestead>
2016-11-13 03:30:05 +0000

    Arreglo problema del editado de varias causas existentes

--- app/Api/Repositories/RepoIngresoCausaExistente.php
+++ app/Api/Repositories/RepoIngresoCausaExistente.php
@@ -17,6 +17,15 @@ class RepoIngresoCausaExistente {
     {
         $array_causas_existentes = explode(",",$causas_existentes);
         $data['ingreso_id'] = $ingreso_id;
+        $model = new IngresosCausasExistentes();
+        $anteriores = $model->where("ingreso_id",$ingreso_id)
+                            ->whereNotIn("causa_existente_id",$array_causas_existentes)
+                            ->get();
+        foreach($anteriores as $causa_anterior)
+        {
+            $causa_anterior->delete();
+        }
+
         foreach($array_causas_existentes as $causa_existente_id)
         {
                 $data = [];
@@ -24,6 +33,7 @@ class RepoIngresoCausaExistente {
                 $data['causa_existente_id'] = $causa_existente_id;
 //                $model = new IngresosCausasExistentes($data);
                 $model = new IngresosCausasExistentes();
+//                $model->where('ingreso_id',$ingreso_id)->delete();
                 $model = $model->firstOrNew(['ingreso_id' => $ingreso_id,'causa_existente_id' => $causa_existente_id]);
 //                dd($model);
                 $model->save();
